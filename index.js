9355f649a47cecef8e476622adaf9357d389b7d5
arthurcai54 <60411904+arthurcai54@users.noreply.github.com>
2021-11-22 15:11:25 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const PORT = process.env.PORT || 8007;
+const app = express();
+const fs = require("fs").promises;
+
+// Don't worry about these 4 lines below
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  fs.readFile("./database.json", "utf8")
+    .then((content) => JSON.parse(content))
+    .then((obj) => console.log(obj.users))
+    .catch((err) => console.log(err));
+  res.render("createcard");
+});
+
+app.post("/create", (req, res) => {
+  console.log(req.body);
+  fs.readFile("./database.json", "utf-8")
+    .then((content) => JSON.parse(content))
+    .then((obj) => console.log(obj.users.push(req.body)))
+
+    .then(()=> fs.writeFile("./database.json", "utf-8"))
+  .catch((err) => console.log(err));
+  res.render("createcard");
+}) 
+
+app.get("/people/:id", (req, res) => {
+  res.render("people");
+});
+
+app.get("/:id/photos", (req, res) => {
+  const id = req.params.id;
+});
+
+app.listen(PORT, () => {
+  console.log(`Server now is running at http://localhost:${PORT} 🚀`);
+});
